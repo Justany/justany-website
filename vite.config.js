892a2b3d85cf83e57edfd7587d272f13f1579163
Justany ITOUA <justany2014@gmail.com>
2024-12-21 02:10:24 +0100

    update dep's

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import path from 'path';
-
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
@@ -23,7 +22,7 @@ export default defineConfig({
             webp: {
                 lossless: true,
             },
-        })
+        }),
     ],
     resolve: {
         alias: {
